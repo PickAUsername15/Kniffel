bbb19a5f1619455a33648f88ae280fa31a7b93b5
PickAUsername15 <rotdob16@new.edvhtl.at>
2018-10-15 20:33:05 +0200

    KniffelRow finished

--- Jg3_010_Kniffel/src/KniffelRow.java
+++ Jg3_010_Kniffel/src/KniffelRow.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JCheckBox;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,28 @@
  */
 public class KniffelRow {
 
+    private String spiel;
+    private JCheckBox wahl;
+    private int points;
+
+    public KniffelRow(String spiel, JCheckBox wahl, int points) {
+        this.spiel = spiel;
+        this.wahl = wahl;
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getSpiel() {
+        return spiel;
+    }
+
+    public JCheckBox getWahl() {
+        return wahl;
+    }
+    
+    
+    
 }
